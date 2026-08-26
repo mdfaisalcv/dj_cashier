86f73f8d9b5b1a839ed28a4ad74a5acb87ba0b3a
Md Faisal Hossain <faisal.hossain@transcombd.com>
2026-08-26 07:45:21 +0600

    Prepare Django project for deployment

--- ApexSuite/settings.py
+++ ApexSuite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,14 +93,14 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3308',
     },
-    'source_db': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'old_database',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3308',
-    },
+    # 'source_db': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'old_database',
+    #     'USER': 'root',
+    #     'PASSWORD': 'root',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3308',
+    # },
 }
 
 
